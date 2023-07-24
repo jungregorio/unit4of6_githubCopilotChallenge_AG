339b90a9348e901202a59333c32cb75e53a56e7c
Armando Gregorio <armando.gregorio@servicestream.com.au>
2023-07-24 13:17:16 +1000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selected
